24704145bfbb71cd7ad21211a97e5d2e112cfefc
Hyun Woo Kim <hynwkm@gmail.com>
2025-06-26 23:21:43 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+    return (
+        <>
+            <header className="w-full flex justify-center">
+                YGO - Culprit Hunt: Find the culprit in your deck!
+            </header>
+            <main>render cards here</main>
+            <footer></footer>
+        </>
+    );
+}
